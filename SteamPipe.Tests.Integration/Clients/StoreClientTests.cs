6c1c01febab973cdbe6e8b204b9b5b466fb4e4e1
Michael Peppler <1872960+peppage@users.noreply.github.com>
2024-09-14 15:27:19 -0400

    Move news tests to store tests

--- SteamPipe.Tests.Integration/Clients/StoreClientTests.cs
+++ SteamPipe.Tests.Integration/Clients/StoreClientTests.cs
@@ -68,6 +68,27 @@
                 Assert.Equal("game", appData.Type);
                 Assert.Equal("Indie Game: The Movie", appData.Name);
             }
+
+            [Fact]
+            public async Task GetNews()
+            {
+                var steam = Helper.GetAuthClient();
+
+                var response = await steam.Api.GetNewsAsync(440, count: 1);
+
+                Assert.True(response.IsSuccess);
+
+                var appNews = response.Body.AppNews;
+
+                Assert.Equal((uint)440, appNews.AppId);
+                Assert.NotEqual(0, appNews.Count);
+                Assert.NotEmpty(appNews.NewsItems);
+
+                var news = appNews.NewsItems[0];
+
+                Assert.NotEqual("", news.Title);
+                Assert.NotEqual("", news.Contents);
+            }
         }
     }
 }
